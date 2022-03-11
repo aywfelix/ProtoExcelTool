6d1b2c5e5f355f5c2e93df9057b0b883468a0f44
Addision <laijia2008@126.com>
2022-03-12 00:51:14 +0800

    更新枚举处理界面

--- program/uilogic/modify_enum.py
+++ program/uilogic/modify_enum.py
@@ -27,7 +27,18 @@ class ModifyEnumUI(QMainWindow):
         self.setWindowOpacity(0.96)
         self.setFixedSize(self.width(), self.height())
 
+        self.ui.tBvEnum.setColumnCount(3)
+        self.ui.tBvEnum.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)    
+        self.ui.tBvEnum.horizontalHeader().setSectionsClickable(False)
+        self.ui.tBvEnum.horizontalHeader().resizeSection(0, 80)
+        self.ui.tBvEnum.horizontalHeader().resizeSection(1, 173)
+        self.ui.tBvEnum.horizontalHeader().resizeSection(2, 175)
+        self.ui.tBvEnum.setShowGrid(True)
+        
         self.parent = parent
         # 添加关联事件
-
+        self.ui.bTnEnumModify.clicked.connect(self.enumModify)
+        pass
+    
+    def enumModify(self):
         pass
