4f593c0d23790f906de308883324b491728d57df
laijia <laijia2008@126.com>
2022-03-31 15:34:15 +0800

    添加导出go加载配置表模板文件

---
+++ program/transtable/templatego.go
@@ -0,0 +1,66 @@
+package Table
+
+import (
+	"encoding/json"
+	io "io/ioutil"
+	"fmt"
+)
+
+type activityRow struct {
+	Id int
+	Open_in_week []int
+}
+
+type activityTable struct {
+	keys  []int
+	table map[int]*activityRow
+}
+
+var ActivityTable *activityTable
+
+func (t *activityTable) GetRow(id int) *activityRow {
+	row, ok := t.table[id]
+	if !ok {
+		return nil
+	}
+	return row
+}
+
+func (t *activityTable) HasRow(id int) bool {
+	_, ok := t.table[id]
+	return ok
+}
+
+func (t *activityTable) Keys() []int {
+	return t.keys
+}
+
+func (t *activityTable) Table() map[int]*activityRow {
+	return t.table
+}
+
+func (t *activityTable) Load() {
+	data, err := io.ReadFile("./Activity.json")
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(data, &t.table)
+	if err != nil {
+		fmt.Println("read table Activity error, ", err.Error())
+		return
+	}
+	for k, _ := range t.table {
+		t.keys = append(t.keys, k)
+	}
+}
+
+func (t *activityTable) Reload() {
+	t.keys = make([]int, 0)
+	t.table = make(map[int]*activityRow)
+	t.Load()
+}
+
+func init() {
+	ActivityTable = new(activityTable)
+	ActivityTable.Load()
+}
